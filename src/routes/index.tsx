b06212a255ff74c9b4876c893d829d9b9a635c97
Natalie Abrams <natalieabrams@Mac.attlocal.net>
2025-04-30 18:44:26 -0400

    add most of the boiler plate routes and route pages. W.I.P.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,106 +1,14 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { Form } from "@builder.io/qwik-city";
-import { useSignIn } from "./plugin@auth";
-
-import Counter from "../components/starter/counter/counter";
-import Hero from "../components/starter/hero/hero";
-import Infobox from "../components/starter/infobox/infobox";
-import Starter from "../components/starter/next-steps/next-steps";
+import { useSignIn } from "~/routes/plugin@auth";
+import { Button } from "@kunai-consulting/kunai-design-system";
 
 export default component$(() => {
   const signInSig = useSignIn();
 
   return (
     <>
-      <Hero />
-      <Starter />
-
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
-
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
-      </div>
-
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
-          </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
-
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
-
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.dev/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/QwikDev/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.dev/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
-        </div>
-      </div>
-
       <Form action={signInSig}>
         <input type="hidden" name="providerId" value="github" />
         <input
@@ -108,8 +16,20 @@ export default component$(() => {
           name="options.redirectTo"
           value="/"
         />
-        <button>Sign In</button>
+        <Button kind="primary">Sign In</Button>
       </Form>
+      <p>
+        <a href="/dashboard">Dashboard</a>
+      </p>
+      <p>
+        <a href="/createRepositories">Create Repositories</a>
+      </p>
+      <p>
+        <a href="/error404">Error 404</a>
+      </p>
+      <p>
+        <a href="/home/login">Login</a>
+      </p>
     </>
   );
 });
