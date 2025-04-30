4c6b91ce111f7bb7d477b7141bc0886382681654
benjamin-kunai <benjamin.hybart@kunaico.com>
2025-04-30 15:51:01 -0400

    Setup Kunai Design System

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { Form } from "@builder.io/qwik-city";
-import { useSignIn } from "./plugin@auth";
-
-import Counter from "../components/starter/counter/counter";
-import Hero from "../components/starter/hero/hero";
-import Infobox from "../components/starter/infobox/infobox";
-import Starter from "../components/starter/next-steps/next-steps";
+import { useSignIn } from "~/routes/plugin@auth";
 import { Button } from "@kunai-consulting/kunai-design-system";
 
 export default component$(() => {
@@ -21,7 +16,7 @@ export default component$(() => {
           name="options.redirectTo"
           value="/"
         />
-        <Button>Sign In</Button>
+        <Button kind="primary">Sign In</Button>
       </Form>
     </>
   );
