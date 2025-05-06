d1004a6f19574e020d99390770e50639343e7d71
benjamin-kunai <benjamin.hybart@kunaico.com>
2025-05-06 16:23:00 -0400

    Fix gradient styles

--- src/components/page/page.tsx
+++ src/components/page/page.tsx
@@ -2,10 +2,10 @@ import { component$, Slot } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    //TODO: CANT FIGURE OUT WHY THE GRADIENT IS NOT WORKING
+    //TODO: Fixed gradient background.  Need to apply background color for dark mode.
     <div
-      class="min-h-screen flex flex-col relative bg-gradient-light dark:bg-gradient-dark
- dark:text-white  text-gray-900 "
+      class="min-h-screen flex flex-col relative bg-[image:var(--background-image-gradient-light)] dark:bg-[image:var(--background-image-gradient-dark)]
+ dark:text-white  text-gray-900 bg-kunai-blue-200 dark:bg-kunai-blue-900"
     >
       <Slot name="header"></Slot>
       <div class="flex flex-grow justify-center items-center relative pt-16 pb-16">
